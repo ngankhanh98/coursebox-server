0f22e8dc9378f20c81bb25bfa0b0e5015c6c04ab
ngankhanh98 <ngankhanh98@gmail.com>
2020-11-16 10:23:34 +0700

    switch to remote db

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -1,19 +1,21 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { localMySql } from 'src/configs/database/mysql.config';
+import { localMySql, remoteMySql } from 'src/configs/database/mysql.config';
 // import { localDBConfig } from 'src/configs/database.config';
 import { Teachers } from '../entities/teachers.entity';
 
 export const DatabaseProvider = TypeOrmModule.forRootAsync({
   imports: [
     ConfigModule.forRoot({
-      load: [localMySql],
+      // load: [localMySql],
+      load: [remoteMySql],
     }),
   ],
   inject: [ConfigService],
   useFactory: (configService: ConfigService): TypeOrmModuleOptions => {
     return {
-      ...configService.get('localMySql'),
+      // ...configService.get('localMySql'),
+      ...configService.get('remoteMySql'),
       entities: [Teachers],
       keepConnectionAlive: true,
       synchronize: true,
